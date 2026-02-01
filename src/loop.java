44f57afcc25c69acf74f225f8c18c607cfdd67f0
joyson <joysonpinto77@gmail.com>
2026-02-01 19:29:57 +0530

    conditon and loop

---
+++ src/loop.java
@@ -0,0 +1,26 @@
+public class loop {
+    public static  void main(String[] args){
+// while , do while, for
+//        for(int i=0;i<10; i++){
+//            System.out.println(" the number is ");
+//        }
+//        int i=0;
+//       while( i<10){
+//           System.out.println("the number is");
+//           i++;
+//       }
+        int i =10;
+
+        do {
+            if (i < 20) {
+                System.out.println("The rate number is 78");
+            }
+         //   i++;
+        } while (i < 90){
+            System.out.print(" the number is while loop");
+            i++;
+        }
+
+
+    }
+}
