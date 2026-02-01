1a048071cf2628d32695697f481d6d0f5347181a
joyson <joysonpinto77@gmail.com>
2026-02-01 17:28:44 +0530

    operator,typecasting

---
+++ src/Operator.java
@@ -0,0 +1,15 @@
+public class Operator {
+
+    public static void main (String[] args){
+// = ,+ ,>,<, -,!
+        int number1 = 40;
+        int number2 = 89;
+        System.out.println(number1+number2);
+        System.out.println(number1-number2);
+        System.out.println(number1/number2);
+        System.out.println(number1%number2);
+    }
+}
+//Arithmetic Operators ( + - , *, /)
+//Relational / Numerical Operators (== =>, !=, <= ,>,<)
+//Logical Operators( && , || ,`)
---
+++ src/Typecasting.java
@@ -0,0 +1,14 @@
+public class Typecasting {
+
+    public static void main(String[] args){
+
+        // converting  the type casting
+        // double value store in the in integer
+        // float < int
+        //
+        float a = 77.439859754f;
+       int b = (int) a;
+       System.out.println(b);
+
+    }
+}
