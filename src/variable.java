89c3389a3463dae5c42015e578eb429d4d23bec7
joyson <joysonpinto77@gmail.com>
2026-02-01 16:37:03 +0530

    variable

---
+++ src/variable.java
@@ -0,0 +1,16 @@
+public class variable {
+    public  static void main(String[] args){
+        //Data type
+        // variable //primitive and non primitive
+        // int , long , float, char,  double,boolean
+        // non primitive are // array // object , function
+
+        // primitve
+        // int , float, char,boolean
+        //non primitive
+
+        int number1 = 90;
+        int number2 = 80;
+         System.out.println(number1+number2);
+    }
+}
