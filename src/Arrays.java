708c4bfb31687bfaacd5b0bfdc341240f616da3e
joyson <joysonpinto77@gmail.com>
2026-02-06 18:53:56 +0530

    today 06-02-2026

---
+++ src/Arrays.java
@@ -0,0 +1,40 @@
+//class Student{
+//    int rollno;
+//    String name;
+//    String email;
+//}
+//
+//
+//public class Arrays {
+//    public static  void  main(String[] args){
+//       Student st = new Student();
+//       st.email =AA" j@1";
+//       st.name ="jo";
+//       st.rollno=89;
+//
+//        Student st2 = new Student();
+//        st2.email =" j@1";
+//        st2.name ="jo";
+//        st2.rollno=89;
+//
+//        Student st3 = new Student();
+//        st3.email =" j@1";
+//        st3.name ="jo";
+//        st3.rollno=89;
+//
+//
+//        Student element[] = new Student[3];
+//
+//       element[0] = st;
+//        element[1] = st2;
+//        element[2]=st3;
+//
+//
+//        for(Student ol : element){
+//            System.out.println("I"+ ol);
+//        }
+//
+//
+//
+//    }
+//}
